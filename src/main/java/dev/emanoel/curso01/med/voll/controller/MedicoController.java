d89d2a55a8482ab52f0450e4b3fc1eb3360baf47
emanoelcampos <emanoel.cv@gmail.com>
2023-09-06 04:59:11 -0300

    interface repository para fazer persistência de dados

--- src/main/java/dev/emanoel/curso01/med/voll/controller/MedicoController.java
+++ src/main/java/dev/emanoel/curso01/med/voll/controller/MedicoController.java
@@ -1,6 +1,10 @@
 package dev.emanoel.curso01.med.voll.controller;
 
+import dev.emanoel.curso01.med.voll.endereco.Endereco;
 import dev.emanoel.curso01.med.voll.medico.DadosCadastroMedico;
+import dev.emanoel.curso01.med.voll.medico.Medico;
+import dev.emanoel.curso01.med.voll.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository repository;
+
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroMedico dadosCadastroMedico) {
-        System.out.println(dadosCadastroMedico);
+    public void cadastrar(@RequestBody DadosCadastroMedico dados) {
+        repository.save(new Medico(dados));
     }
 }
---
+++ src/main/java/dev/emanoel/curso01/med/voll/medico/MedicoRepository.java
@@ -0,0 +1,6 @@
+package dev.emanoel.curso01.med.voll.medico;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicoRepository extends JpaRepository<Medico, Long> {
+}
