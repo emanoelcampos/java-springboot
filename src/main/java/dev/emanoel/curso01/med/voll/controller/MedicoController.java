84299db99f59b3101198b411e4d1c6fb3871baa9
emanoelcampos <emanoel.cv@gmail.com>
2023-09-08 22:46:32 -0300

    paginação do retorno de lista de medicos

--- src/main/java/dev/emanoel/curso01/med/voll/controller/MedicoController.java
+++ src/main/java/dev/emanoel/curso01/med/voll/controller/MedicoController.java
@@ -6,6 +6,8 @@ import dev.emanoel.curso01.med.voll.medico.Medico;
 import dev.emanoel.curso01.med.voll.medico.MedicoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,10 +27,9 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<DadosListagemMedico> listar() {
-        return repository.findAll()
-                .stream()
-                .map(DadosListagemMedico::new)
-                .toList();
+    public Page<DadosListagemMedico> listar(Pageable paginacao) {
+        return repository
+                .findAll(paginacao)
+                .map(DadosListagemMedico::new);
     }
 }
