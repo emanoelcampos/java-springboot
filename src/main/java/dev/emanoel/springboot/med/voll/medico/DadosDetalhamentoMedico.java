bd1e5a9f00d0cacb39a6ff7b9ed1db0f1c1e211e
emanoelcampos <emanoel.cv@gmail.com>
2023-09-18 01:11:29 -0300

    padronizando retornos da API

---
+++ src/main/java/dev/emanoel/springboot/med/voll/medico/DadosDetalhamentoMedico.java
@@ -0,0 +1,21 @@
+package dev.emanoel.springboot.med.voll.medico;
+
+import dev.emanoel.springboot.med.voll.endereco.Endereco;
+
+public record DadosDetalhamentoMedico(Long id,
+                                      String nome,
+                                      String email,
+                                      String crm,
+                                      String telefone,
+                                      Especialidade especialidade,
+                                      Endereco endereco) {
+    public DadosDetalhamentoMedico(Medico medico) {
+        this(medico.getId(),
+                medico.getNome(),
+                medico.getEmail(),
+                medico.getCrm(),
+                medico.getTelefone(),
+                medico.getEspecialidade(),
+                medico.getEndereco());
+    }
+}
