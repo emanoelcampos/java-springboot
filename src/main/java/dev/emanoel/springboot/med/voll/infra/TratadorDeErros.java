f7ba8a190bd733981816738790279b97ed38352f
emanoelcampos <emanoel.cv@gmail.com>
2023-09-19 07:29:48 -0300

    tratando erro 404

---
+++ src/main/java/dev/emanoel/springboot/med/voll/infra/TratadorDeErros.java
@@ -0,0 +1,16 @@
+package dev.emanoel.springboot.med.voll.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class TratadorDeErros {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarErro404() {
+        return ResponseEntity.notFound().build();
+    }
+
+}
