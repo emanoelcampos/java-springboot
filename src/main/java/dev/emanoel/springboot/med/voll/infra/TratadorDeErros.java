8ed1f5e6d18c396c4302886cd7c09c066f7b1ed7
emanoelcampos <emanoel.cv@gmail.com>
2023-09-20 23:54:44 -0300

    tratando erro 400 de forma simples sem corpo de resposta

--- src/main/java/dev/emanoel/springboot/med/voll/infra/TratadorDeErros.java
+++ src/main/java/dev/emanoel/springboot/med/voll/infra/TratadorDeErros.java
@@ -2,6 +2,7 @@ package dev.emanoel.springboot.med.voll.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,6 +12,12 @@ public class TratadorDeErros {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity tratarErro404() {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400() {
+
+        return ResponseEntity.badRequest().build();
     }
 
 }
