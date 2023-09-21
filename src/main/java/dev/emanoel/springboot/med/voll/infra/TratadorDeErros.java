6e1f3db388c6c08730a0d541853af2d5b1d84fd3
emanoelcampos <emanoel.cv@gmail.com>
2023-09-20 23:57:28 -0300

    tratando erro 400 de com corpo de resposta com todas as informações

--- src/main/java/dev/emanoel/springboot/med/voll/infra/TratadorDeErros.java
+++ src/main/java/dev/emanoel/springboot/med/voll/infra/TratadorDeErros.java
@@ -15,9 +15,9 @@ public class TratadorDeErros {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity tratarErro400() {
-
-        return ResponseEntity.badRequest().build();
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException exception) {
+        var erros =  exception.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros);
     }
 
 }
