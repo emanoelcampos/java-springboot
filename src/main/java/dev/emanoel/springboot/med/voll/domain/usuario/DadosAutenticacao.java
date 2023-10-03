72ea2ab3104d73672c0b6d60fe8ec40add790107
emanoelcampos <emanoel.cv@gmail.com>
2023-10-02 22:52:24 -0300

    controller de autenticação

---
+++ src/main/java/dev/emanoel/springboot/med/voll/domain/usuario/DadosAutenticacao.java
@@ -0,0 +1,4 @@
+package dev.emanoel.springboot.med.voll.domain.usuario;
+
+public record DadosAutenticacao(String login, String senha) {
+}
--- src/main/java/dev/emanoel/springboot/med/voll/domain/usuario/Usuario.java
+++ src/main/java/dev/emanoel/springboot/med/voll/domain/usuario/Usuario.java
@@ -5,6 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity(name = "Usuario")
 @Table(name = "usuarios")
@@ -12,11 +18,46 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor //construtor padrão sem argumentos, jpa exige em todas as entidades
 @AllArgsConstructor //construtor que recebe todos os campos
 @EqualsAndHashCode(of = "id") //gerar equals e hashcode em cima do id e não em de todos os atributos
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String login;
     private String senha;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
